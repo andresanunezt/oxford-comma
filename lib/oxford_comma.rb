8ade38bd365b746ac509b8a385f95700043a383e
Andrés Nuñez <andresanunez98@gmail.com>
2021-02-25 19:31:55 -0500

    commit tree 5974bff2179d4c7527ee6315bb29f4679d1b3f20

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
+# require 'pry'
 def oxford_comma(array)
-
+#    binding.pry
+if array.length == 1
+    array.join
+elsif array.length == 2
+     array.join(" and ")
+elsif array.length >= 3
+   last_element = array.pop
+    array.join(", ") << (", and #{last_element}")
+end
 end
